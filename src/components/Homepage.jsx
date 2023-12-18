280217e2ce2e027e60e410e0102ba63a22b239ab
jonas <jonasabelmellado@gmail.com>
2023-12-18 10:57:24 -0500

    moved img files to poublic to preview on netifly

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -9,6 +9,7 @@ const Homepage = ({user}) => {
             <br/>
             <p> Word of wisdom: Kindness is free to give but at the same time one of the most valuable thing one can receive...It does not collect interest if saved up and not used. Best gift you can give is unconditional kindness because that is what you desire... embody the behaviours you want to see in others because a society without kindness is no society at all     " some athiest, not that long ago "</p>
             <br/>
+            <img src="public/zoolander.jpeg"/>
         </div>
     )
 }
